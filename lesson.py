8760abeb48777e380960c168628e1a4e61a7a26f
minomoyusei <ym11322567@gmail.com>
2023-08-24 22:03:32 +0900

    文字列の操作

--- lesson.py
+++ lesson.py
@@ -1,9 +1,48 @@
-import math
+print('hello')
+print("hello")
 
-result = math.sqrt(25)
-print(result)
+print("I don't know")
+print('I don\'t know')
 
-y = math.log2(10)
-print(y)
+print('say "I don\'t know"')
+print("say \"I don't know\"")
 
-print(help(math))
+print('hello. How are you?')
+print('hello.\nHow are you?')
+
+print('C:\\name\\name')
+
+print('################')
+print("""
+line1
+line2
+line3
+""")
+print('################')
+
+print('################')
+print("""\
+line1
+line2
+line3\
+""")
+print('################')
+
+print('HI.' * 3 + 'Mike.')
+s = ('aaaaaaaaaaaaaaaaaaaaaa'
+     'bbbbbbbbbbbbbbbbbbbbbb') #長い文章の時は''を2つ使う場合がある
+print(s)
+
+word = 'python'
+print(word[0])
+print(word[1])
+print(word[-1])
+print(word[0:2])
+print(word[2:5])
+print(word[:])
+print(word[:2])
+print(word[2:])
+word = 'j' + word[1:]
+print(word)
+n = len(word)
+print(n)
