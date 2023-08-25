7f0ef560a531f6160723cc4c29ccdd158dd6a058
minomoyusei <ym11322567@gmail.com>
2023-08-25 11:17:20 +0900

    文字列のメソッド

--- lesson.py
+++ lesson.py
@@ -1,48 +1,13 @@
-print('hello')
-print("hello")
-
-print("I don't know")
-print('I don\'t know')
-
-print('say "I don\'t know"')
-print("say \"I don't know\"")
-
-print('hello. How are you?')
-print('hello.\nHow are you?')
-
-print('C:\\name\\name')
-
-print('################')
-print("""
-line1
-line2
-line3
-""")
-print('################')
-
-print('################')
-print("""\
-line1
-line2
-line3\
-""")
-print('################')
-
-print('HI.' * 3 + 'Mike.')
-s = ('aaaaaaaaaaaaaaaaaaaaaa'
-     'bbbbbbbbbbbbbbbbbbbbbb') #長い文章の時は''を2つ使う場合がある
+s = 'My name is Mike. Hi Mike'
 print(s)
+is_start = s.startswith('My')
+print(is_start)
 
-word = 'python'
-print(word[0])
-print(word[1])
-print(word[-1])
-print(word[0:2])
-print(word[2:5])
-print(word[:])
-print(word[:2])
-print(word[2:])
-word = 'j' + word[1:]
-print(word)
-n = len(word)
-print(n)
+print(s.find('Mike'))
+print(s.rfind('Mike'))
+print(s.count('Mike'))
+print(s.capitalize())
+print(s.title())
+print(s.upper())
+print(s.lower())
+print(s.replace('Mike', 'Nancy'))
