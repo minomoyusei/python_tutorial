ba5a81ecc5977815319f87b5ce081db13a79d663
minomoyusei <ym11322567@gmail.com>
2023-08-25 12:36:43 +0900

    文字列の代入

--- lesson.py
+++ lesson.py
@@ -1,13 +1,5 @@
-s = 'My name is Mike. Hi Mike'
-print(s)
-is_start = s.startswith('My')
-print(is_start)
+a = 'a'
+print(f'a is {a}')
 
-print(s.find('Mike'))
-print(s.rfind('Mike'))
-print(s.count('Mike'))
-print(s.capitalize())
-print(s.title())
-print(s.upper())
-print(s.lower())
-print(s.replace('Mike', 'Nancy'))
+x, y, z = 1, 2, 3
+print(f'a is {x}, {y}, {z}')
