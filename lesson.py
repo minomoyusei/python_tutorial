49fd4a9d69e8acccdc942a8ed4c4d4af8882bd19
minomoyusei <ym11322567@gmail.com>
2023-08-26 16:18:24 +0900

    リストのメソッド

--- lesson.py
+++ lesson.py
@@ -1,58 +1,23 @@
-l = [1, 20, 4, 50, 2, 1, 2]
-print(l[0])
-print(l[1])
-print(l[-1])
-print(l[0:2])
-print(l[:])
-print(len(l))
-print(type(l))
-print(list('abcdefg'))
-n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(n[::2])
-print(n[::-1])
+r = [1, 2, 3, 4, 5, 1, 2, 3]
+print(r.index(3, 3))
 
-a = ['a', 'b', 'c']
-n = [1, 2, 3]
-x = [a, n]
-print(x)
-print(x[0])
-print(x[0][1])
+print(r.count(3))
 
-s = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-print(s)
-s[0] = 'X'
-print(s)
-s[2:5] = ['C', 'D', 'E']
-print(s)
-s[5:] = []
-print(s)
+if 5 in r:
+    print('exist')
 
-n = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-print(n)
-n.append(100)
-print(n)
-n.insert(0, 100)
-print(n)
-n.pop()
-print(n)
-n.pop(0)
-print(n)
-del n[0]
-print(n)
-del n
-n = [1, 2, 2, 2, 3]
-n.remove(2)
-print(n)
-n.remove(2)
-n.remove(2)
-print(n)
-a = [1, 2, 3, 4, 5]
-b = [6, 7, 8, 9, 10]
-x = a + b
+r.sort()
+print(r)
+r.sort(reverse=True)
+print(r)
+r.reverse()
+print(r)
+
+s = 'My name is Mike.'
+to_split = s.split(' ')
+print(to_split)
+
+x = ' '.join(to_split)
 print(x)
-a += b
-print(a)
-x = [1, 2, 3, 4, 5]
-y = [6, 7, 8, 9, 10]
-x.extend(y)
-print(x)
+
+print(help(list))
