e6a7abc030583c5da5d90076d27c9b4af8d1ce09
minomoyusei <ym11322567@gmail.com>
2023-08-27 23:52:43 +0900

    リストの使用例

--- lesson.py
+++ lesson.py
@@ -1,29 +1,15 @@
-i = [1, 2, 3, 4, 5]
-j = i
-j[0] = 100
-print('j = ', j)
-print('i = ', i)
-
-
-x = [1, 2, 3, 4, 5]
-y = x.copy()
-y[0] = 100
-print('y = ', y)
-print('x = ', x)
-
-X = 10
-Y = X
-print(id(Y))
-Y = 5
-print(Y)
-print(X)
-print(id(X))
-print(id(Y))
-
-X = ['a', 'b']
-Y = X
-Y[0] = 'p'
-print(id(X))
-print(id(Y))
-print(Y)
-print(X)
+seat = []
+min = 0
+max = 5
+print(min <= len(seat) < max)
+seat.append('p')
+print(min <= len(seat) < max)
+print(len(seat))
+seat.append('p')
+seat.append('p')
+seat.append('p')
+print(min <= len(seat) < max)
+seat.append('p')
+print(min <= len(seat) < max)
+seat.pop(0)
+print(min <= len(seat) < max)
