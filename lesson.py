686a0e89cb62361117bdf47fa1a9a9a880dff213
minomoyusei <ym11322567@gmail.com>
2023-08-28 23:33:55 +0900

    タプル型

--- lesson.py
+++ lesson.py
@@ -1,15 +1,33 @@
-seat = []
-min = 0
-max = 5
-print(min <= len(seat) < max)
-seat.append('p')
-print(min <= len(seat) < max)
-print(len(seat))
-seat.append('p')
-seat.append('p')
-seat.append('p')
-print(min <= len(seat) < max)
-seat.append('p')
-print(min <= len(seat) < max)
-seat.pop(0)
-print(min <= len(seat) < max)
+t = (1, 2, 3, 4, 1, 2)
+print(t)
+print(type(t))
+print(t[-1])
+print(t.index(1))
+print(t.index(1,1))
+print(t.count(1))
+print(help(list))
+print(help(tuple))
+
+t = ([1, 2, 3], [4, 5, 6])
+print(t)
+t[0][0] = 100
+print(t)
+
+t = 1, 2, 3
+print(t)
+t = 1,
+print(t)
+print(type(t))
+
+t = ()
+print(t)
+print(type(t))
+
+t = (1)
+print(t)
+print(type(t))
+
+new_tuple = (1, 2, 3) + (4, 5, 6)
+print(new_tuple)
+new_tuple = (1,) + (4, 5, 6)
+print(new_tuple)
