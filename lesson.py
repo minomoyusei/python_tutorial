da40fb9db4b41aa3fcb444f908a007833c67995d
minomoyusei <ym11322567@gmail.com>
2023-08-31 12:45:37 +0900

    辞書型

--- lesson.py
+++ lesson.py
@@ -1,8 +1,14 @@
-chose_from_two = ('A', 'B', 'C')
-
-answer = []
-answer.append('A')
-answer.append('A')
-
-print(chose_from_two)
-print(answer)
+d = {'x':10, 'y': 20}
+print(d)
+print(type(d))
+print(d['x'])
+d['x'] = 100
+print(d['x'])
+d['x'] = 'XXXX'
+print(d)
+d['z'] = 200
+print(d)
+d[1] = 10000
+print(d)
+print(dict(a=10,b=20))
+dict([('a',10),('b',20)])
