ad33a6327e942b21f2b30d3e84b2dfb7c261ded1
minomoyusei <ym11322567@gmail.com>
2023-09-05 23:43:50 +0900

    辞書型のコピー

--- lesson.py
+++ lesson.py
@@ -1,25 +1,15 @@
-d = {'x': 10, 'y': 20}
-help(d)
-print(d.keys())
-print(d.values())
-d2 = {'x': 1000, 'j': 500}
-print(d)
-print(d2)
-d.update(d2)
-print(d)
-print(d['x'])
-print(d.get('x'))
-print(d.get('z'))
-r = d.get('z')
-print(r)
-print(type(r))
-print(d.pop('x'))
-print(d)
-del d['y']
-print(d)
-del d
-d = {'a': 100, 'b': 200}
-d.clear()
-print(d)
-d = {'a': 100, 'b': 200}
-print('a' in d)
+x = {'a':1}
+y = x
+y['a'] = 1000
+print(x)
+print(y)
+print(id(x))
+print(id(y))
+
+x = {'a':1}
+y = x.copy()
+y['a'] = 1000
+print(x)
+print(y)
+print(id(x))
+print(id(y))
