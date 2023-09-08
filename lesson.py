bd8c14a6a1be3aa2bb0450050a484cda9f38c2b8
minomoyusei <ym11322567@gmail.com>
2023-09-08 23:56:39 +0900

    集合のメソッド

--- lesson.py
+++ lesson.py
@@ -1,12 +1,14 @@
-a = {1, 2, 2, 3, 4, 4, 4, 5, 6}
-print(a)
+s = {1, 2, 3, 4, 5}
+print(s)
+s.add(6)
+print(s)
+s.add(6)
+print(s)
+s.remove(6)
+print(s)
+s.clear()
+print(s)
+a = {}
 print(type(a))
+print(help(set))
 
-b = {2, 3, 3, 6, 7}
-print(b)
-
-print(a - b)
-print(b - a)
-print(a & b)
-print(a | b)
-print(a ^ b)
